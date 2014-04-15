0095207029d9e10777ccc6844146af77950e7a89
Ygam Retuta <ygam.j.retuta@gmail.com>
2014-04-15 15:44:59 +0800

    fix typo in dalli config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -53,7 +53,7 @@ Mbb::Application.configure do
 
   # Use a different cache store in production.
   if ENV['MEMCACHEDCLOUD_SERVERS']
-    config.cache_store :dalli_store, ENV['MEMCACHEDCLOUD_SERVERS'].split(','),
+    config.cache_store = :dalli_store, ENV['MEMCACHEDCLOUD_SERVERS'].split(','),
                        {username:ENV['MEMCACHEDCLOUD_USERNAME'],
                        password:ENV['MEMCACHEDCLOUD_PASSWORD']}
   end
